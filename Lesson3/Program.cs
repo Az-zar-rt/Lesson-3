5df7430fd3beb8a86b1da78fca9f2e96b394767f
Az-zar-rt <beloshapka2001.09.03@gmail.com>
2022-08-07 01:20:30 +0300

    fistcommit

---
+++ Lesson3/Program.cs
@@ -0,0 +1,28 @@
+﻿int x = 11;
+int y = 11;
+int sum = 0;
+
+Console.WriteLine("Первое число "+x);
+Console.WriteLine("Второе число " +y);
+if (x > y)
+{
+    for (int i = y; i < x + 1; i++)
+    {
+        sum += i;
+        Console.WriteLine(sum);
+    }
+}
+else if (y > x)
+{
+    for (int i = x; i < y + 1; i++)
+    {
+        sum += i;
+        Console.WriteLine(sum);
+    }
+}
+else
+{
+    x = y;
+    sum = x;
+    Console.WriteLine(sum);
+}
